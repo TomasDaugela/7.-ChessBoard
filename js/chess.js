58ae6b363865cb2d8c175ffd1917f9c860414cd8
Tomas <daugeltomas@gmail.com>
2020-05-04 15:18:17 +0300

    commit

---
+++ js/chess.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+function renderChessboard (size) {
+    let HTML = '';
+    const DOMchessboard= document.querySelector('.chessboard')
+
+    for (let i=0; i<size;i++) {
+        
+    }
+        
+    
+
+}
+
+
+
